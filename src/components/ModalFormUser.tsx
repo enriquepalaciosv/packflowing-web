5313b917d1280aeecdca405c4d10d7de6bcab71b
Lisandro <salvareschilisandro0@gmail.com>
2025-05-19 22:56:33 +0000

    Create modal for form user

---
+++ src/components/ModalFormUser.tsx
@@ -0,0 +1,22 @@
+import { Usuario } from "../firebase/firestore/usuarios";
+import FormUser from "./FormUser";
+import Modal from "./Modal";
+
+export default function ModalFormUser({
+  entity,
+  isOpen,
+  onClose,
+}: {
+  entity: Usuario;
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <Modal
+      open={isOpen}
+      onClose={onClose}
+      title={"Editar usuario"}
+      content={<FormUser onClose={onClose} entity={entity} />}
+    />
+  );
+}
